e7569bedadf9be7010cad4702050949e0a17c87b
Arydeep Sah <aryasah19@gmail.com>
2026-05-12 20:04:47 +0530

    Cretae EC2 instance

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "sgtf" {
+  name        = "sgterraform"
+  description = "allow for ssh"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-011cefff1c767b619"
+  instance_type          = "t3.micro"
+  key_name               = "chat"
+  vpc_security_group_ids = [aws_security_group.sgtf.id]
+
+  tags = {
+    Name = "terrformEC2"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
