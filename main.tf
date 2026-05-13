dba501ff01e09a8605c2b7b09cc78842e63b39f1
Jatin thakur <jatuthakur170@gmail.com>
2026-05-13 10:57:26 +0530

    Update security group to allow HTTP traffic
    Added HTTP ingress rule to security group.

--- main.tf
+++ main.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "sgtf" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "my_ec2" {
